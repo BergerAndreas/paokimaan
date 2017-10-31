acceaffb556d0291ad3515b3b1d3c02f864b965f
haavarop <haavard@vaage.com>
2017-10-31 13:40:14 +0100

    Add ng g account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonComponent
+    PokemonComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
