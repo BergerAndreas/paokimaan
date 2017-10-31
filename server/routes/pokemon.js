4d92749432732e1f062c30c7920fc014c0a50e5f
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-10-31 14:43:45 +0100

    Create api view for single pokemon by id

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://anton:anton@ds241395.mlab.com:41395/pokeweebs', ['pokemon']);
 
-
+//Get All Pokemon
 router.get('/pokemon', function(req, res, next){
     db.pokemon.find(function(err, pokemon){
+        if(err){
+            res.send(err);
+        }
+        res.json(pokemon);
+    });
+});
+
+//Get single pokemon
+router.get('/pokemon/:id', function(req, res, next){
+    db.pokemon.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, pokemon){
         if(err){
             res.send(err);
         }
