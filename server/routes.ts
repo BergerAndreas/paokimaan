22425655829e48d2980a2beb2ae50f4a11c3d9f8
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-11-07 17:24:44 +0100

    Small fix

---
+++ server/routes.ts
@@ -0,0 +1,33 @@
+import * as express from 'express';
+
+import PokemonCtrl from './controllers/pokemon';
+import UserCtrl from './controllers/user';
+import Pokemon from './models/pokemon';
+import User from './models/user';
+
+export default function setRoutes(app) {
+
+  const router = express.Router();
+
+  const pokemonCtrl = new PokemonCtrl();
+  const userCtrl = new UserCtrl();
+
+  // Cats
+  router.route('/pokemon').get(pokemonCtrl.getAll);
+  router.route('/pokemon/count').get(pokemonCtrl.count);
+  router.route('/pokemon/:id').get(pokemonCtrl.get);
+
+
+  // Users
+  router.route('/login').post(userCtrl.login);
+  router.route('/users').get(userCtrl.getAll);
+  router.route('/users/count').get(userCtrl.count);
+  router.route('/user').post(userCtrl.insert);
+  router.route('/user/:id').get(userCtrl.get);
+  router.route('/user/:id').put(userCtrl.update);
+  router.route('/user/:id').delete(userCtrl.delete);
+
+  // Apply the routes to our application with the prefix /api
+  app.use('/api', router);
+
+}
