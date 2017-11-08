cf34d42b5125a3de92f8be98a02e0ba484da7b8b
bortne <kristsbo@stud.ntnu.no>
2017-11-08 17:53:23 +0100

    Add navbar with route to Pokemon and Account

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,20 @@ import { MatTableModule, MatListModule } from '@angular/material'
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { AccountComponent } from './account/account.component';
+import { NavbarComponent } from './navbar/navbar.component'
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule, MatCardModule, MatMenuModule } from '@angular/material';
+
+
+//Route
+import {Routes, RouterModule} from '@angular/router';
+
+const AppRoutes : Routes = [
+  {path: 'pokemon', component:PokemonComponent},
+  {path: 'profile', component: AccountComponent}
+]
 
 //Services
 import { PokemonService } from "./services/pokemon.service";
@@ -15,13 +29,20 @@ import { PokemonService } from "./services/pokemon.service";
   declarations: [
     AppComponent,
     PokemonComponent,
-    AccountComponent
+    AccountComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [
     PokemonService
