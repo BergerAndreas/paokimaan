dfb0fda7ca77741cec2ed1348621973c4d739bdb
Erikkvo <Erikkvo@hotmail.com>
2017-11-09 13:39:25 +0100

    Merge user and navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,24 +11,6 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AccountComponent } from './account/account.component';
-
-import { NavbarComponent } from './navbar/navbar.component'
-
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import { MatButtonModule, MatCardModule, MatMenuModule } from '@angular/material';
-
-
-//Route
-import {Routes, RouterModule} from '@angular/router';
-
-const AppRoutes : Routes = [
-  {path: 'pokemon', component:PokemonComponent},
-  {path: 'profile', component: AccountComponent}
-]
-
-//Services
-import { PokemonService } from "./services/pokemon.service";
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {PokemonComponent} from "./pokemon/pokemon.component";
@@ -38,8 +20,6 @@ import { MatTableModule, MatListModule } from '@angular/material'
 @NgModule({
   declarations: [
     PokemonComponent,
-    AccountComponent,
-    NavbarComponent,
     AppComponent,
     RegisterComponent,
     LoginComponent,
@@ -52,13 +32,7 @@ import { MatTableModule, MatListModule } from '@angular/material'
     RoutingModule,
     SharedModule,
     MatListModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatMenuModule,
-    RouterModule.forRoot(AppRoutes)
+    MatTableModule
   ],
   providers: [
     AuthService,
--- client/src/app/routing.module.ts
+++ client/src/app/routing.module.ts
@@ -20,6 +20,7 @@ const routes: Routes = [
   { path: 'logout', component: LogoutComponent },
   { path: 'account', component: AccountComponent, canActivate: [AuthGuardLogin] },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuardAdmin] },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
